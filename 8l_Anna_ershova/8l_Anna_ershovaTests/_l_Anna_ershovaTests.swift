851e9b875e9e297dc00998a4c4537d63a77ec7ef
Anna Ershova <anechka_ershova@macbook-air-anna.local>
2018-12-17 01:21:44 +0000

    homework_8_snake

---
+++ 8l_Anna_ershova/8l_Anna_ershovaTests/_l_Anna_ershovaTests.swift
@@ -0,0 +1,34 @@
+//
+//  _l_Anna_ershovaTests.swift
+//  8l_Anna_ershovaTests
+//
+//  Created by Anna Ershova on 17/12/2018.
+//  Copyright © 2018 Anna Ershova. All rights reserved.
+//
+
+import XCTest
+@testable import _l_Anna_ershova
+
+class _l_Anna_ershovaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
